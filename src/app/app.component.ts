cab5dede44c92e6abec9e1ce54e2d23fcda50752
nrizika2020 <nrizika2020@chadwiskschool.org>
2019-07-02 15:54:27 -0700

    feat: Add Ability to Put Goals from Firebase in Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthService } from './shared/services/auth.service';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'goal-management-angular';
 
-  constructor(public auth: AuthService){}
+export class AppComponent {
+    title = 'goal-management-angular';
+  constructor(public auth: AuthService) {
+
+  }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../shared/services/home.service';
+import { Observable } from 'rxjs';
 
 export interface PeriodicElement {
   name: string;
@@ -6,10 +8,22 @@ export interface PeriodicElement {
   goalsCompleted: number;
 }
 
+export interface GoalsData {
+  description: string;
+  dueDate: Date;
+  isCompleted: boolean;
+  createdBy: string;
+}
+
 const ELEMENT_DATA: PeriodicElement[] = [
   {name: 'Anastasia', goalsAssigned: 5, goalsCompleted: 5},
   {name: 'Bob', goalsAssigned: 3, goalsCompleted: 2},
   {name: 'Cheryl', goalsAssigned: 16, goalsCompleted: 0},
+];
+
+const GOALS_DATA: GoalsData[] = [
+  {description: 'make table', dueDate: new Date('2019-07-03'), isCompleted: false, createdBy: 'JD'},
+  {description: 'eat chipotle', dueDate: new Date('2019-07-03'), isCompleted: true, createdBy: 'JD'},
 ];
 
 @Component({
@@ -20,11 +34,24 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class HomeComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'goalsAssigned', 'goalsCompleted'];
+  goalsDisplayedColumns: string[] = ['description', 'dueDate', 'isCompleted', 'createdBy'];
   dataSource = ELEMENT_DATA;
+  goalsDataSource = GOALS_DATA;
 
-  constructor() { }
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
+    this.showGoals();
+  }
+
+  showGoals() {
+    this.homeService.getAddedGoals().subscribe( goals => {
+      this.goalsDataSource = goals;
+    });
+  }
+
+  getUser(userId: string): Observable<any> {
+    return this.homeService.getUser(userId);
   }
 
 }
