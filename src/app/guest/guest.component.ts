31ef2994a6d5a979b2928ecd1787bc224b33f083
Angelina Tsuboi <Angelina@Angelinas-MacBook-Air.local>
2019-08-07 10:02:58 -0700

    feat: Update guest page

--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'gms-guest',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuestComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
