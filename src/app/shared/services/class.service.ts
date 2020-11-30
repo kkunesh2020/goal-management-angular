47eb3202219ca1ae98f49f82126a69a1112a4697
Angelina Tsuboi <51937266+angelina-tsuboi@users.noreply.github.com>
2020-11-30 12:24:38 -0800

    Update class.service.ts

--- src/app/shared/services/class.service.ts
+++ src/app/shared/services/class.service.ts
@@ -10,11 +10,15 @@ import UserClass from '../models/user';
 export class ClassService {
   private userCollection: CollectionReference;
   private classCollection: CollectionReference;
+  
+  //initializes users and classes collections
   constructor(private afs: AngularFirestore) {
     this.userCollection = afs.firestore.collection('users');
     this.classCollection = afs.firestore.collection('classes');
    }
 
+  //checks if a student is allowed inside a class using the student's userID and classID
+  // @param userID and classID
    allowedInClass(userID: string, classID: string) {
      let allowed = false;
      this.userCollection.doc(userID).get().then(doc => {
@@ -31,6 +35,8 @@ export class ClassService {
      return allowed;
    }
 
+  //get a student's data with their userID
+  //@param id
    getStudentDataByID(id: string): Promise<UserClass>{
     let promise = this.userCollection.doc(id).get().then((doc) => {
       return new UserClass(id, doc.data().name, doc.data().email,  doc.data().isAdmin,
@@ -39,6 +45,8 @@ export class ClassService {
     return promise;
    }
 
+  //get multiple students data given an array of student document references
+  //@param studentRefs: DocumentReference[]
    getStudentsData(studentRefs: DocumentReference[]) {
     let studentData = [];
     studentRefs.forEach(ref => {
@@ -50,6 +58,8 @@ export class ClassService {
     return studentData;
    }
 
+  //get multiple students data with an array of student UIDs
+  //@param studentUID: string[]
    async getStudentsDataByID(studentUID: string[]): Promise<any[]> {
     let studentData = [];
     for(let uid of studentUID){
@@ -59,6 +69,8 @@ export class ClassService {
     return studentData;
    }
 
+  //get an individual student's data with a documentReference
+  //@param ref: DocumentReference
    getStudentData(ref: DocumentReference): Promise<any> {
      let promise = ref.get().then(doc => {
        return doc.data();
@@ -66,6 +78,7 @@ export class ClassService {
      return promise;
    }
 
+//  helper method for getting length of array
    getLengthOf(array: any[]) {
     if(array == null) {
       return 0;
@@ -73,6 +86,8 @@ export class ClassService {
     return array.length;
   }
 
+// given an array of student document references get the student data for each ref.
+// @param refs: DocumentReference[]
    async getStudentsDataByReference(refs: DocumentReference[]): Promise<any> {
     let studentsData: StudentData[] = [];
     for(let studentRef of refs){
@@ -84,6 +99,9 @@ export class ClassService {
     return studentsData;
    }
 
+  
+// get a user's class using their uid
+// @param userID: string
    getClasses(userID: string): Class[] {
     const classes: Class[] = [];
     console.log('getting classes for user');
@@ -98,6 +116,8 @@ export class ClassService {
     return classes;
   }
 
+  // get a teacher;s class with their uid and the class id
+  // @param teacherUID: string, classID: string
   getClass(teacherUID: string, classID: string): Promise<any> {
     let promise = this.classCollection.doc(classID).get().then(doc => {
       if (doc.exists) {
