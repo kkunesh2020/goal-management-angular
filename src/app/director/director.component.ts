4e2e24e8ab688a58d19c2c721bd58534b4e39d79
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-11 12:48:23 -0700

    fix: init director and create class component to resolve app module errors

---
+++ src/app/director/director.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gms-director',
+  templateUrl: './director.component.html',
+  styleUrls: ['./director.component.scss']
+})
+export class DirectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
