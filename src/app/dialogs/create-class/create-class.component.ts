09691d423c38987f8df91dfc6a719793a5171980
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-11 21:51:07 -0700

    feat: added base code to create class dialog

--- src/app/dialogs/create-class/create-class.component.ts
+++ src/app/dialogs/create-class/create-class.component.ts
@@ -1,4 +1,10 @@
+import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import DirectorClassClass from 'src/app/shared/models/directorClass';
+import { DirectorClass } from 'src/app/shared/models/directorClass.model';
+import { GoalService } from 'src/app/shared/services/goal.service';
+import { CreateGoalComponent } from '../create-goal/create-goal.component';
 
 @Component({
   selector: 'gms-create-class',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateClassComponent implements OnInit {
 
-  constructor() { }
+  class: DirectorClass;
+  loading = false;
+  assignedStudentID: string[] = [];
+  assignedToAll: boolean;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private goalService: GoalService,
+              public dialogRef: MatDialogRef<CreateGoalComponent>) {
+    this.class = new DirectorClassClass(0, '', '', [], '');
+
+   }
 
   ngOnInit() {
+  // initially assign to all students
+    this.assignAllStudents();
+  }
+
+  assignAllStudents() {
+    this.assignedStudentID = [];
+    this.data.students.forEach(student => {
+      this.assignedStudentID.push(student.uid);
+    });
+  }
+
+
+  formComplete(): boolean {
+    return this.class.title != "" && this.class.students.length > 0;
+  }
+
+  checkSpecific(studentID: string, assigned: boolean) {
+    if (assigned) {
+      this.assignedStudentID.push(studentID);
+    } else { // removes from assigned student array
+      this.assignedStudentID = this.assignedStudentID.filter(id => id !== studentID);
+    }
+  }
+
+  resetList() {
+    this.assignedToAll = false;
+    console.log(this.assignedToAll);
+    this.assignedStudentID = [];
+  }
+
+
+  createClass() {
+    this.loading = true;
+    // this.goalService.createGoal(this.goal).then(() => {
+    //   this.loading = false;
+    //   this.dialogRef.close('success');
+    // });
   }
 
 }
--- src/app/director/director-view/director-view.component.ts
+++ src/app/director/director-view/director-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
+import { CreateClassComponent } from 'src/app/dialogs/create-class/create-class.component';
+import { CreateGoalComponent } from 'src/app/dialogs/create-goal/create-goal.component';
 import { Class } from 'src/app/shared/models/class.model';
 import { DirectorClass } from 'src/app/shared/models/directorClass.model';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -11,12 +14,14 @@ import { ClassService } from 'src/app/shared/services/class.service';
   styleUrls: ['./director-view.component.scss']
 })
 export class DirectorViewComponent implements OnInit {
+  // TODO: get stream of class data
 
   classes: DirectorClass[];
   loading = true;
   constructor(
     private classService: ClassService,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {
   }
 
@@ -44,13 +49,13 @@ export class DirectorViewComponent implements OnInit {
     //   classID: this.classID,
     //   students: this.classService.getStudentsData(this.class.students),
     // };
-    // // passes in class data into the dialog
-    // const dialogRef = this.dialog.open(CreateGoalComponent, {
-    //   data,
-    //   width: '27rem',
-    //   height: '30rem',
-    //   panelClass: 'custom-modalbox',
-    // });
+    // passes in class data into the dialog
+    const dialogRef = this.dialog.open(CreateClassComponent, {
+      // data,
+      width: '27rem',
+      height: '30rem',
+      panelClass: 'custom-modalbox',
+    });
 
     // dialogRef.afterClosed().subscribe((result) => {
     //   if (result === 'success') {
