200b413def8ba8ffa9ec2c62d87eaabdafee1fcf
angelina-tsuboi <angelina.t1832@gmail.com>
2021-05-23 23:13:43 -0700

    feat: wrote email functions for student/teacher created goal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
-const db = admin.firestore();
 
 // Sendgrid Config
 import * as sgMail from '@sendgrid/mail';
@@ -15,12 +14,8 @@ sgMail.setApiKey(API_KEY);
 // student added to class
 export const studentAddedToClass = functions.https.onCall(async (data, context) => {
 
-    if (!context.auth && !context.auth.token.email) {
-        throw new functions.https.HttpsError('failed-precondition', 'Must be logged with an email address');
-    }
-
     const msg = {
-        to: context.auth.token.email,
+        to: data.email,
         from: 'goalmanagement@chadwickschool.org',
         templateId: TEMPLATE_ID,
         dynamic_template_data: {
@@ -42,35 +37,73 @@ export const studentAddedToClass = functions.https.onCall(async (data, context) 
 });
 
 // student creates goal
-// export const welcomeEmail = functions.auth.user().onCreate(user => {
+export const studentCreatesGoal = functions.https.onCall(async (data, context) => {
 
-//     const msg = {
-//         to: user.email,
-//         from: 'hello@fireship.io',
-//         templateId: TEMPLATE_ID,
-//         dynamic_template_data: {
-//             subject: 'Welcome to my awesome app!',
-//             name: user.displayName,
-//         },
-//     };
+    if (!context.auth && !context.auth.token.email) {
+        throw new functions.https.HttpsError('failed-precondition', 'Must be logged with an email address');
+    }
 
-//     return sgMail.send(msg);
+    let msg = {
+        to: data.email,
+        // TODO: Use an actual domain.
+        from: {
+          name: 'Chadwick School Goal Management',
+          email: 'goalmanagement@chadwickschool.org'
+        },
+        subject: `You have a new goal ${data.goal}!`,
+        text: `You have a new goal ${data.goal} for ${data.class} created by ${data.teacher}. Please visit ${URL}/goals to accept the goal.`,
+        html: `
+        <p>Hey there!</p>
+        <p>
+         You have a <b>new goal ${data.goal}</b> for ${data.class} created by ${data.teacher}.
+          Please <a href="${URL}/goals">click here</a> to read it.
+        </p>
+        <p>
+          - Chadwick School Goal Management
+        </p>`,
+      }
 
-// });
+    await sgMail.send(msg);
+
+    // Handle errors here
+
+    // Response must be JSON serializable
+    return { success: true };
+
+});
 
 // teacher creates goal
-// export const welcomeEmail = functions.auth.user().onCreate(user => {
+export const teacherCreatesGoal = functions.https.onCall(async (data, context) => {
 
-//     const msg = {
-//         to: user.email,
-//         from: 'hello@fireship.io',
-//         templateId: TEMPLATE_ID,
-//         dynamic_template_data: {
-//             subject: 'Welcome to my awesome app!',
-//             name: user.displayName,
-//         },
-//     };
+    if (!context.auth && !context.auth.token.email) {
+        throw new functions.https.HttpsError('failed-precondition', 'Must be logged with an email address');
+    }
 
-//     return sgMail.send(msg);
+    let msg = {
+        to: data.email,
+        // TODO: Use an actual domain.
+        from: {
+          name: 'Chadwick School Goal Management',
+          email: 'goalmanagement@chadwickschool.org'
+        },
+        subject: `${data.student} created a goal!`,
+        text: `Your student, ${data.student}, created <b> a goal called ${data.goal} for ${data.class}. </b> Please <a href="${URL}/goals">click here</a> to check it out.`,
+        html: `
+        <p>Hi ${data.teacher}!</p>
+        <p>
+          Your student, ${data.student}, created <b> a goal called ${data.goal} for ${data.class}. </b>
+          Please <a href="${URL}/goals">click here</a> to check it out.
+        </p>
+        <p>
+          - Chadwick School Goal Management
+        </p>`,
+      }
 
-// });
+    await sgMail.send(msg);
+
+    // Handle errors here
+
+    // Response must be JSON serializable
+    return { success: true };
+
+});
