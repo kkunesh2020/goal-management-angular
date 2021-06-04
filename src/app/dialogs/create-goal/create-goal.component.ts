d01136db257d8f1b308082ef18d27c97dc9b8a79
angelina-tsuboi <angelina.t1832@gmail.com>
2021-06-04 08:38:45 -0700

    feat: finished date validators for create goal

--- src/app/dialogs/create-goal/create-goal.component.ts
+++ src/app/dialogs/create-goal/create-goal.component.ts
@@ -54,9 +54,16 @@ export class CreateGoalComponent implements OnInit {
   }
 
   checkDateErrors(): boolean {
-    if (this.goal.dueDate < new Date()) {
-      this.dateErrors = "Due date cannot be before or during today";
-      return true;
+    if(this.goal.dueDate){
+      let beforeDateOffset = new Date(this.goal.dueDate.getTime() + (24*60*60*1000));
+      let afterDateOffset = new Date(this.goal.dueDate.getTime() - (24*60*60*1000 * 14));
+      if (beforeDateOffset < new Date()) {
+        this.dateErrors = "Due date cannot be before or during today";
+        return true;
+      }else if(afterDateOffset > new Date()){
+        this.dateErrors = "Due date must be within 2 weeks";
+        return true;
+      }
     }
     return false;
   }
