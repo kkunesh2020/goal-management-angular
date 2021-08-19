38133cb5938a2ae698eb2f37bc5c6ec87aae4079
angelina-tsuboi <angelina.t1832@gmail.com>
2021-08-18 23:16:17 -0700

    feat: modified icons on update goal modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ import { UpdateClassComponent } from './dialogs/update-class/update-class.compon
 import { DeleteClassComponent } from './dialogs/delete-class/delete-class.component';
 import { WarningPendingComponent } from './dialogs/warning-pending/warning-pending.component';
 import { ViewTeacherRejectionComponent } from './dialogs/view-teacher-rejection/view-teacher-rejection.component';
-import { NbThemeModule, NbLayoutModule, NbButtonModule, NbIconModule, NbCardModule, NbDialogModule, NbDialogService, NbSpinnerModule, NbDialogRef, NbDatepickerModule, NbInputModule, NbTreeGridModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbIconModule, NbCardModule, NbDialogModule, NbDialogService, NbSpinnerModule, NbDialogRef, NbDatepickerModule, NbInputModule, NbTreeGridModule, NbListModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { SpinnerComponent } from './spinner/spinner.component';
 
@@ -133,7 +133,8 @@ import { SpinnerComponent } from './spinner/spinner.component';
     NbInputModule,
     NbSpinnerModule,
     NbDatepickerModule.forRoot(),
-    NbTreeGridModule
+    NbTreeGridModule,
+    NbListModule
   ],
   providers: [NbDialogService],
   bootstrap: [AppComponent],
